c52e0faf0a5b2714925027dba6b161776e9a103e
Jovan Stojanovski <jovan.stojanovski55@gmail.com>
2024-03-04 18:15:30 +0100

    Add logger support for my POC project

--- rest-api-spring-boot/rest-api-spring-boot/src/main/java/com/poc/restapispringboot/controller/TestController.java
+++ rest-api-spring-boot/rest-api-spring-boot/src/main/java/com/poc/restapispringboot/controller/TestController.java
@@ -2,12 +2,16 @@ package com.poc.restapispringboot.controller;
 
 import com.poc.restapispringboot.dto.TestDTO;
 import com.poc.restapispringboot.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    private static Logger logger = LoggerFactory.getLogger(TestController.class);
     private TestService testService;
 
     @Autowired
@@ -17,6 +21,7 @@ public class TestController {
 
     @GetMapping("/test")
     public TestDTO getName(){
+        logger.info("Calling TestController");
         return testService.getName();
     }
 }
